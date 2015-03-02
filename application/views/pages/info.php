8163f6a48edbe9d9467888b4433f0d5caef35b25
Guy Moore <guy.moore.normal@gmail.com>
2015-03-02 11:45:50 -0800

    Added info landing page with a jumbotron and empty nav elements.

---
+++ application/views/pages/info.php
@@ -0,0 +1,13 @@
+<div class="container" >
+    
+    <div class="jumbotron
+        <h1>Guy Moore</h1>
+        <p class="lead">
+            
+            Computer Science major at Cuesta College. I'm knowledgeble in programming, and networking, and I love learning new things.
+
+        </p>
+    </div><!-- end jumbotron -->
+
+</div><!-- end container -->
+
