7cbbdaa252403ea00eea5fd0186da1bddec2622a
Guy Moore <guy.moore.normal@gmail.com>
2015-03-02 13:18:43 -0800

    Fixed nav items.

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,12 +1,13 @@
+			<hr>
+			<footer
 
-		<hr>
-		<footer
+				<p> &copy Guy Moore 2015 </p>	
 
-			<p> &copy Guy Moore 2015 </p>	
-
-		</footer
-
-		</div> <!-- end container. -->
+			</footer
+		</div> <!-- end container -->
 
 	</body>
+	
+	<script src="js/bootstrap.min.js"></script>
+
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,30 +1,35 @@
 <!DOCTYPE html>
-	<html>
-		<head>
-			<meta charset="utf-8">
-			<meta http-equiv="X-UA-Compatible" content="IE=edge">
-			<meta name="viewport" content="width=device-width, initial-scale=1">
-			<title><?php echo $title;?> | Guy Moore</title>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title><?php echo $title;?> | Guy Moore</title>
 
-			<!-- Bootstrap -->
-			<link href="css/bootstrap.min.css" rel="stylesheet">
-		</head>
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="jumbotron-narrow.css" rel="stylesheet">	
+	</head>
 
-		<body>
-			
-			<div calss="container">
-			
-				<div class="header">
+	<body>
+		
+		<div class="container">
 
-					<nav>
+			<div class="header">
+
+				<nav class="navbar navbar-default">	
+
+						<div class="navbar-header">	
+							<a href="#" class="navbar-brand">Guy Moore</a>		
+						</div>
+
+						<ul class="nav navbar-nav pull-right">
+							<li role="presentation" ><a href="#">Info</a></li>
+							<li role="Presentation" ><a href="#">Contact</a></li>
+						</ul>		
+
+				</nav>	
 			
-						<ul class="nav nav-pills">
-							
-							<li role="presentation" >Info</li>
-							<li role="Presentation" >Contact</li>
-							
-						</ul>
-						
-					</nav>
-				
-				</div> <!-- end header -->
+			</div> <!-- end header -->
+
+			<hr>
