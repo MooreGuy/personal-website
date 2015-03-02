518fcb0c48f54f285cdff120fd61a346b0b2d61c
Guy Moore <guy.moore.normal@gmail.com>
2015-03-02 11:53:31 -0800

    Fixed system and application paths.

--- www/index.php
+++ www/index.php
@@ -56,7 +56,7 @@ if (defined('ENVIRONMENT'))
  * as this file.
  *
  */
-	$system_path = '../CodeIgniter-2.2-stable/system';
+	$system_path = '../system';
 
 /*
  *---------------------------------------------------------------
@@ -72,7 +72,7 @@ if (defined('ENVIRONMENT'))
  * NO TRAILING SLASH!
  *
  */
-	$application_folder = '../CodeIgniter-2.2-stable/application';
+	$application_folder = '../application';
 
 /*
  * --------------------------------------------------------------------
