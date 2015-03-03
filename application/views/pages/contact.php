884a24dcf71f0dac5368f7078d1baaddec8a9d71
Guy Moore <guy.moore.normal@gmail.com>
2015-03-02 20:21:55 -0800

    Added contact view.

---
+++ application/views/pages/contact.php
@@ -0,0 +1,26 @@
+<div class="row howto">
+	<div class="col-md-12">
+		<h3>How to Contact Me</h3>
+		<p>Normally you can get a hold of me by email, but if you're into the social media type of mediums, then twitter and Google+ are the best ways.</p>
+	</div>
+</div>
+
+<div class="row  contact">
+
+	<div class="col-md-6">
+		<h4><i class="fa fa-envelope-o"></i><a href="mailt:guy.moore.normal@gmail.com">Email</a></h4>
+
+		<p>I'll try to get to you as soon as I can, but I'm very busy.</p>
+	</div>
+
+	<div class="col-md-6">
+		<h4><i class="fa fa-twitter"></i><a href="https://twitter.com/MooreNormal">Twitter</a></h4>
+		<p>Go ahead and follow me.</p>
+	</div>
+
+	<div class="col-md-6">
+		<h4><i class="fa fa-google-plus"></i><a href="https://plus.google.com/u/0/+GuyMoore/posts">Google+</a></h4>
+		<p>Go ahead and follow me.</p>
+	</div>
+
+</div>
