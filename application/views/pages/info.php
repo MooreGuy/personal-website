be703bb5ca271c944da73637be59ae56e3815670
Guy Moore <guy.moore.normal@gmail.com>
2015-03-02 20:18:31 -0800

    Completed nav and profile links.

--- application/views/pages/info.php
+++ application/views/pages/info.php
@@ -1,8 +1,28 @@
 <div class="jumbotron">
-	<h1>Guy Moore</h1>
+	<h2>About Me</h2>
 	<p class="lead">
 		
 		Computer Science major at Cuesta College. I'm knowledgeble in programming, and networking, and I love learning new things.
 
 	</p>
+
+	<button class="btn btn-lg btn-success" href="#" role="button">Hire Me</button>
 </div><!-- end jumbotron -->
+
+<div class="row projectlinks" >
+
+	<div class="col-lg-6" >
+		<h4><i class="fa fa-github"></i><a href="https://github.com/MooreGuy">Github Profile</a></h4>
+		<p>
+			Although, I can't be as active here as I would like because of school there are still some interesting projects.
+		</p>
+	</div>
+
+	<div class="col-lg-6" >
+		<h4><i class="fa fa-bitbucket"></i><a href="https://bitbucket.com/MooreGuy">Bitbucket Profile</a></h4>
+		<p>
+			Most projects here are private.	
+		</p>
+	</div>
+
+</div> <!-- end row projectlinks -->
