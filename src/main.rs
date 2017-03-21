a84f321342d1d0910f7fc1899844d0715d86fd9e
Guy Moore <guy.moore.normal@gmail.com>
2017-03-21 00:21:37 -0700

    Add templates

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
 extern crate iron;
 
-use iron::prelude::*;
-use iron::status;
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
+use std::error::Error;
+use iron::{Iron, Request, Response, IronResult, status};
+
+//use router::Router;
 
 fn main() {
+    fn get_file() -> &str {
+        let mut path = Path::new("pages/home.html");
+        let mut file = match File::open(&path) {
+            Ok(file) => file,
+            Err(reason) => panic!("couldn't do the thing"),
+        };
+        let mut s = String::new();
+        let mut readFile = match file.read_to_string(&mut s) {
+            Ok(file_string) => s,
+            Err(reason) => {
+                panic!("couldn't do the thing")
+            },
+        };
+    }
+
     fn hello_world(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "Welcome to Guy Moore's website!")))
+        Response::with((status::Ok, get_file()))
     }
 
     println!("Listening on port 3000");
