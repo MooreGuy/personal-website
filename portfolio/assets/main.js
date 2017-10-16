558e2e013ae87b8dba2c6cbce900b97d75708312
Guy Moore <guy.moore.normal@gmail.com>
2017-10-16 08:26:37 +0000

    Add cube webgl starting point

---
+++ portfolio/assets/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    var canvas = document.getElementById("decision-cube");
+    if (!canvas) {
+        console.log("Couldn't retrieve canvas.");
+        return;
+    }
+});
